02552e73f9094db504c47a234203cb673a83c1c5
amang06 <aman@skynetiks.com>
2025-06-10 11:18:11 +0530

    setImmediate

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,4 +1,4 @@
-export const REFRESH_TIME_IN_MILLISECONDS = 5000
-export const MAX_NUMBER_OF_MESSAGES_TO_PROCESS = 20 // In Seconds
-export const VISIBILITY_TIMEOUT = 30  // Must be approx time takes in seconds to delete a message from SQS 
+// export const REFRESH_TIME_IN_MILLISECONDS = 1000
+export const MAX_NUMBER_OF_MESSAGES_TO_PROCESS = 10 // In Seconds
+export const VISIBILITY_TIMEOUT = 60  // Must be approx time takes in seconds to delete a message from SQS 
 export const WAIT_TIME_SECONDS = 3 // In Seconds
